1b48e2f7ac9019f9dcd2edf4d46aef765655e2cf
steveperreault <perreaultstevej@gmail.com>
2016-08-29 15:17:06 -0400

    Add files via upload

---
+++ chapter3/ex3-3.cpp
@@ -0,0 +1,27 @@
+// ex3-3.cpp -- Uses three user-defined functions to print a nursery rhyme
+
+#include<iostream>
+
+void threeblind(void);
+void seehow(void);
+
+int main()
+{
+	threeblind();
+	threeblind();
+	seehow();
+	seehow();
+
+	std::cin.get();
+	std::cin.get();
+
+	return 0;
+}
+
+void threeblind(void) {
+	std::cout << "Three blind mice." << std::endl;
+}
+
+void seehow(void) {
+	std::cout << "See how they run." << std::endl;
+}
