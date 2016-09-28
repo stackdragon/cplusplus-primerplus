ad9735f2534af20781ea6bf6b15a2dd9598f43b8
Steve <perreaultstevej@gmail.com>
2016-09-27 20:06:32 -0400

    Create ex4-1.cpp

---
+++ Chapter4/ex4-1.cpp
@@ -0,0 +1,25 @@
+//  ex4-1.cpp -- Accept strings with multiple words; Increment char type
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string firstName;
+	std::string lastName;
+	char grade;
+	int age;
+
+	std::cout << "What is your first name? ";
+	std::getline(std::cin, firstName);
+	std::cout << "What is your last name? ";
+	std::getline(std::cin, lastName);
+	std::cout << "What is your age? ";
+	std::cin >> age;
+	std::cout << "What letter grade do you deserve? ";
+	std::cin >> grade;
+	std::cout << "Name: " << lastName << ", " << firstName << std::endl;
+	std::cout << "Grade: " << static_cast<char>(grade + 1) << std::endl;
+	std::cout << "Age: " << age << std::endl;
+
+}
