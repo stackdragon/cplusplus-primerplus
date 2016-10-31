9b1eb51f444456529f511cd33dc50082944dbf54
Steve <perreaultstevej@gmail.com>
2016-10-31 16:47:05 -0400

    Create ex7-2.cpp

---
+++ Chapter7/ex7-2.cpp
@@ -0,0 +1,61 @@
+//  ex7-2.cpp Write a program that asks the user to enter up to 10 golf scores,
+//  which are to be stored in an array. You should provide a means for the user
+//  to terminate input prior to entering 10 scores. The program should
+//  display all the scores on one line and report the average score. Handle input, 
+//  display, and the average calculation with three separate array - processing
+//  functions.
+
+#include <iostream>
+
+const int maxSize = 10;
+int fillArray(int scores[], int maxSize);
+void showArray(const int scores[], int size);
+float showAverage(const int scores[], int size);
+
+int main()
+{
+	int scores[maxSize];
+	int size = fillArray(scores, maxSize);		
+	showArray(scores, size);
+	std::cin.get();
+}
+
+int fillArray(int scores[], int maxSize)		// get input from user
+{
+	std::cout << "Enter your golf scores. Enter a letter to stop input.\n";
+	int i = 0;
+	for (i = 0; i < maxSize; i++)
+	{
+		std::cout << "Enter score #" << i + 1 << ": ";
+		std::cin >> scores[i];
+		if (!std::cin)	// if input is bad
+		{
+			std::cin.clear();	// clear error flag for future erros
+			std::cout << "Ending input.\n";
+			break;
+		}
+	}
+	return i;		// return the number of scores input
+}
+
+void showArray(const int scores[], int size)
+{
+	std::cout << "\nScores: ";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << scores[i];
+		if (i < size-1)
+			std::cout << ", ";
+		else
+			std::cout << ".\n";		// end with period if last score in array
+	}
+	std::cout << "Average score: " << showAverage(scores, size) << std::endl;
+}
+
+float showAverage(const int scores[], int size)
+{
+	int total = 0;
+	for (int i = 0; i < size; i++)
+		total += scores[i];
+	return total / size;	// returns the average score
+}
