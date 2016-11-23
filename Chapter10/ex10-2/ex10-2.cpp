9524c6afb95a853e6a9c1fa297a383ad4d97ea9a
Steve <perreaultstevej@gmail.com>
2016-11-22 20:19:12 -0500

    Update ex10-2.cpp

--- Chapter10/ex10-2/ex10-2.cpp
+++ Chapter10/ex10-2/ex10-2.cpp
@@ -1,1 +1,20 @@
+// ex10-2.cpp Write a program that completes the supplied implementation by providing 
+// code for the undefined methods.
 
+#include <iostream>
+#include <string>
+#include "definitions.h"
+
+int main()
+{ 
+	Person one;		// use default constructor 
+	Person two("Smythecraft");	   // use #2 with one default argument 
+	Person three("Dimwiddy", "Sam");  // use #2, no defaults
+	one.Show(); 
+	one.FormalShow(); // etc. for two and three
+	two.Show();
+	two.FormalShow();
+	three.Show();
+	three.FormalShow();
+	return 0;
+}
