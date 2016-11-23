c06c53d6ab6d13134c9f4a18070c16e88c237fe6
Steve <perreaultstevej@gmail.com>
2016-11-22 19:58:20 -0500

    Update definitions.h

--- Chapter10/ex10-1/definitions.h
+++ Chapter10/ex10-1/definitions.h
@@ -11,7 +11,9 @@ private:
 	std::string m_accountNum;
 	double m_balance;
 public:
+	BankAccount(); // default constructor
 	BankAccount(const std::string &name, const std::string &accountNum, double balance = 0);
+	~BankAccount(); // destructor
 	void deposit(double money);
 	void withdraw(double money);
 	void display();
