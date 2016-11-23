7c20f61ea7edc3f4ef83caaccfc4cdad45626f10
Steve <perreaultstevej@gmail.com>
2016-11-22 19:52:34 -0500

    Update ex10-1.cpp

--- Chapter10/ex10-1/ex10-1.cpp
+++ Chapter10/ex10-1/ex10-1.cpp
@@ -1,1 +1,39 @@
+// ex10-1.cpp Provide method definitions for the class described in Chapter Review 
+// Question 5 and write a short program that illustrates all the features. 
 
+#include <iostream>
+#include <string>
+#include "definitions.h"
+
+int main()
+{ 
+	BankAccount myAccount { "Acme Co", "12345", 10000.00 };
+
+	int option = 0;
+	std::cout << "1. Deposit funds\n2. Withdraw funds\n3. Display balance\n\n";
+	std::cout << "Select an option (q to quit): ";
+	std::cin >> option;
+	while (std::cin)
+	{
+		double amount = 0;
+		if (option == 1)
+		{
+			std::cout << "Enter amount to deposit: ";
+			std::cin >> amount;
+			myAccount.deposit(amount);
+		}
+		else if (option == 2)
+		{
+			std::cout << "Enter amount to withdraw: ";
+			std::cin >> amount;
+			myAccount.withdraw(amount);
+		}
+		else if (option == 3)
+			myAccount.display();
+		else
+			std::cout << "Invalid input, try again. \n";
+		std::cout << "Select an option (q to quit): ";
+		std::cin >> option;
+	}
+	return 0;
+}
