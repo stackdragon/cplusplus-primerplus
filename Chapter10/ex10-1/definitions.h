3555b62e64638052f33cb0e3b115d5abeca936d7
Steve <perreaultstevej@gmail.com>
2016-11-22 19:53:14 -0500

    Create definitions.h

---
+++ Chapter10/ex10-1/definitions.h
@@ -0,0 +1,18 @@
+// compile with ex10-1.cpp and implements.cpp
+#pragma once
+
+// header files
+#include <string>
+
+class BankAccount
+{
+private:
+	std::string m_name;
+	std::string m_accountNum;
+	double m_balance;
+public:
+	BankAccount(const std::string &name, const std::string &accountNum, double balance = 0);
+	void deposit(double money);
+	void withdraw(double money);
+	void display();
+};
