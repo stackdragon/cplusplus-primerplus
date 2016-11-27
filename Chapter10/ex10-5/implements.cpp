d08be260f59baa6b1a6107567352ea5105e64ffc
Steve <perreaultstevej@gmail.com>
2016-11-26 19:26:32 -0500

    Create implements.cpp

---
+++ Chapter10/ex10-5/implements.cpp
@@ -0,0 +1,38 @@
+// compile with definitions.h and ex10-5.cpp
+#include <iostream>
+#include "definitions.h"
+
+Stack::Stack()    // create an empty stack 
+{ 
+	top = 0; 
+}
+
+bool Stack::isempty() const 
+{ 
+	return top == 0; 
+}
+
+bool Stack::isfull() const 
+{ 
+	return top == MAX; 
+}
+
+bool Stack::push(const Item & item) 
+{
+	if (top < MAX) 
+	{ 
+		items[top++] = item; 
+		return true; 
+	}
+	else return false;
+}
+
+bool Stack::pop(Item & item) 
+{ 
+	if (top > 0) 
+	{ 
+		item = items[--top]; 
+		return true; 
+	} 
+	else return false; 
+}
