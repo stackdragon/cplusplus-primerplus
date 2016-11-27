01a3e6e00a11545a90a9765920a82fe0d5b70ce2
Steve <perreaultstevej@gmail.com>
2016-11-26 21:52:15 -0500

    Create list.h

---
+++ Chapter10/ex10-8/list.h
@@ -0,0 +1,20 @@
+// compile with ex10-8.cpp and list.cpp
+#pragma once
+#include <string>
+
+typedef int Item;
+
+class List
+{
+private:
+	enum { MAX = 5 };   // constant specific to class 
+	Item m_items[MAX];    // holds list items
+	int m_top;            // index for top list items
+public:
+	List(); // constructor creates an empty list
+	bool push(const Item & item);
+	bool isEmpty() const;
+	bool isFull() const;
+	void show() const;
+	void visit(void(*pSquare)(Item & item));
+};
