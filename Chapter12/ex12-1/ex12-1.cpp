e150e1825edcb63ec11cdf158bb41b770cba7234
Steve <perreaultstevej@gmail.com>
2016-12-02 22:40:23 -0500

    Update ex12-1.cpp

--- Chapter12/ex12-1/ex12-1.cpp
+++ Chapter12/ex12-1/ex12-1.cpp
@@ -1,1 +1,16 @@
+// ex12-1.cpp  Provide the implementation for a provided class and write a short program 
+// that uses all the member functions. 
 
+#include <iostream> 
+#include <cstring>
+#include "cow.h"
+
+int main() 
+{ 
+	Cow cow1;							// create first object via default constructor
+	Cow cow2("Abe", "Eating", 200);		// create second object via constructor
+	Cow cow3 = cow2;					// create third object using copy constructor
+	Cow cow4("Bob", "Sleeping", 300);	// create fourth object via constructor
+	cow3 = cow4;						// Reassign third object via overloaded assignment operator
+	return 0;
+}
