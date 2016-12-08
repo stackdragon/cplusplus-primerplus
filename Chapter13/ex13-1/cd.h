50467df99c6c3bf91688fecb47a451e9de03d184
Steve <perreaultstevej@gmail.com>
2016-12-07 21:52:23 -0500

    Create cd.h

---
+++ Chapter13/ex13-1/cd.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// base class 
+class Cd
+{  
+	// represents a CD disk 
+private: 
+	char performers[50]; 
+	char label[20]; 
+	int selections;   // number of selections 
+	double playtime;  // playing time in minutes 
+public: 
+	Cd(char * s1, char * s2, int n, double x); // explicit constructor
+	Cd(const Cd & d); // copy constructor
+	Cd(); // default constructor
+	virtual ~Cd(); // destructor
+	virtual void Report() const;  // reports all CD data 
+	Cd & operator=(const Cd & d); // overloaded assignment operator
+};
